576403960d15793dcd9c01890ac11d08a9b2d8bf
Joyce <joyce81116@gmail.com>
2019-08-09 15:51:08 +0800

    20190809_16

--- index.php
+++ index.php
@@ -1,31 +1,10 @@
 <body>
     <h1>登入頁面</h1>
     <form action="" method="post">
-        <div>帳號：<input type="text" name="account" /></div>
-        <div>密碼：<input type="text" name="pass" /></div>
+        <div>會員登入<input type="text" name="account" /></div>
+        <div>註冊會員<input type="text" name="pass" /></div>
         <input type="submit" name="btnOK" value="登入" />
     </form>
 </body>
 
 
-
-<?php 
-session_start();
-$link = mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
-mysqli_select_db($link, "user");
-
-@$account = $_POST["account"];
-@$pass = $_POST["pass"];
-$sql = "select pass from user_account WHERE account = '{$account}'";
-$arr = mysqli_query($link,$sql);
-$row = mysqli_fetch_assoc($arr);
-
-if(empty($account) || empty($pass)){
-    echo "請登入";
-}elseif(!empty($account) && !empty($pass) && $row['pass'] == $_POST["pass"]){
-    header("location:lab02.php");
-}else {
-    echo "帳號密碼錯誤";
-}
-
-?>
---
+++ login.php
@@ -0,0 +1,34 @@
+<body>
+    <h1>登入頁面</h1>
+    <form action="" method="post">
+        <div>帳號：<input type="text" name="account" /></div>
+        <div>密碼：<input type="text" name="pass" /></div>
+        <input type="submit" name="btnOK" value="登入" />
+    </form>
+</body>
+
+
+
+<?php 
+session_start();
+$link = mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
+mysqli_select_db($link, "user");
+
+@$account = $_POST["account"];
+@$pass = $_POST["pass"];
+$sql = "select pass from user_account WHERE account = '{$account}'";
+$arr = mysqli_query($link,$sql);
+$row = mysqli_fetch_assoc($arr);
+
+if(empty($account) || empty($pass)){
+    echo "請登入";
+}elseif(!empty($account) && !empty($pass) && $row['pass'] == $_POST["pass"]){
+    $_SESSION["account"] = $account;
+    header("location:lab02.php");
+}else {
+    echo "帳號密碼錯誤";
+    $ins = "insert into user_account values ('{$account}','{$pass}')";
+    $arr = mysqli_query($link,$ins);
+}
+
+?>
