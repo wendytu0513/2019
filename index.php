56b6e0b7841558b8bd3550d8a17713c3b30a0c9b
Joyce <joyce81116@gmail.com>
2019-08-09 11:55:17 +0800

    20190809

--- index.php
+++ index.php
@@ -13,31 +13,19 @@
 session_start();
 $link = mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
 mysqli_select_db($link, "user");
-$pass="";
-$account="";
-//include("lab02.php");
-//$db = new db;
-$account = $_POST["account"];
-$pass = $_POST["pass"];
+
+@$account = $_POST["account"];
+@$pass = $_POST["pass"];
 $sql = "select pass from user_account WHERE account = '{$account}'";
-//$arr = $db->Query($sql);
 $arr = mysqli_query($link,$sql);
 $row = mysqli_fetch_assoc($arr);
-// if (!empty($account) && !empty($pass) && $pass == $arr ) {
-//     //$_SESSION["account"] = $account;
-//     echo "登入";
-//     //header("location:lab02.php");
-// } else {
-//     echo "登入失敗";
-// } 
 
-if ( !empty($account) && !empty($pass) && $pass = $row) {
-    //$_SESSION["account"] = $account;
-    echo "登入";
+if(empty($account) || empty($pass)){
+    echo "請登入";
+}elseif(!empty($account) && !empty($pass) && $row['pass'] == $_POST["pass"]){
     header("location:lab02.php");
-    echo $row['pass'];
-} else {
-    echo "登入失敗";
-} 
+}else {
+    echo "帳號密碼錯誤";
+}
 
 ?>
--- lab02.php
+++ lab02.php
@@ -1,17 +1,21 @@
 <?php 
-header("content-type:text/html; charset=utf-8");
-$link = @mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
+//session_start();
+// header("content-type:text/html; charset=utf-8");
+// $link = @mysqli_connect("localhost", "root", "") or die(mysqli_connect_error());
 
-mysqli_select_db($link, "user");
-$commandText = "select * from user_account";
-$result = mysqli_query($link, $commandText);
+// mysqli_select_db($link, "user");
+// $commandText = "select * from user_account";
+// $result = mysqli_query($link, $commandText);
 
 
-while ($row = mysqli_fetch_assoc($result))
-{
-  echo "ID：{$row['account']}<br>";
-  echo "Name：{$row['pass']}<br>";
-  echo "<HR>";
-}
+// while ($row = mysqli_fetch_assoc($result))
+// {
+//   echo "ID：{$row['account']}<br>";
+//   echo "Name：{$row['pass']}<br>";
+//   echo "<HR>";
+// }
+include ("index.php");
 
+
+echo $_SESSION["account"];
 ?>
